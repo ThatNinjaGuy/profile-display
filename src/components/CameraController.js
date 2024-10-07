89899a177b42c9e0a0cd0867dc9b659425c6525c
ThatNinjaGuy <vishalgautam.tech@gmail.com>
2024-10-07 21:35:26 +0530

    Go back button managed to allow navigation

--- src/components/CameraController.js
+++ src/components/CameraController.js
@@ -1,20 +1,39 @@
 import React from "react";
 import { useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import gsap from "gsap";
 
 export const CameraController = ({ orbitControlsEnabled }) => {
   const { camera, gl } = useThree();
 
   return (
-    <OrbitControls
-      args={[camera, gl.domElement]}
-      enablePan={orbitControlsEnabled}
-      enableZoom={orbitControlsEnabled}
-      enableRotate={orbitControlsEnabled}
-      minPolarAngle={0}
-      maxPolarAngle={Math.PI}
-      minDistance={5}
-      maxDistance={100}
-    />
+    orbitControlsEnabled && (
+      <OrbitControls enableDamping args={[camera, gl.domElement]} />
+    )
   );
+};
+
+export const moveCamera = (
+  camera,
+  screenPosition,
+  screenRotation,
+  onComplete
+) => {
+  const offset = new THREE.Vector3(0, 0, 10).applyEuler(
+    new THREE.Euler(...screenRotation)
+  );
+  const cameraPosition = new THREE.Vector3(...screenPosition).add(offset);
+
+  gsap.to(camera.position, {
+    duration: 1.5,
+    x: cameraPosition.x,
+    y: cameraPosition.y,
+    z: cameraPosition.z,
+    onUpdate: () => {
+      camera.lookAt(new THREE.Vector3(...screenPosition));
+      camera.updateProjectionMatrix();
+    },
+    onComplete,
+  });
 };
--- src/components/NavigationItems.js
+++ src/components/NavigationItems.js
@@ -2,32 +2,23 @@ import React from "react";
 import { Sphere, Html, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
-import gsap from "gsap";
 import config from "../configs/navigationItems.json";
+import { moveCamera } from "./CameraController";
 
-export const NavigationItems = ({ setOrbitControlsEnabled }) => {
+export const NavigationItems = ({
+  setNavigationItemClicked,
+  openDisplayMenus,
+}) => {
   const envMap = useTexture("/asphalt_04_diff_1k.jpg");
   envMap.mapping = THREE.EquirectangularReflectionMapping;
 
   const { camera } = useThree();
 
   const handleClick = (item) => {
-    setOrbitControlsEnabled(false);
-
-    // Calculate the new camera position in front of the screen
-    const screenPosition = new THREE.Vector3(...item.screenPosition);
-    const screenRotation = new THREE.Euler(...item.screenRotation);
-    const offset = new THREE.Vector3(0, 0, 5).applyEuler(screenRotation);
-    const cameraPosition = screenPosition.clone().add(offset);
-
-    gsap.to(camera.position, {
-      duration: 1.5,
-      x: cameraPosition.x,
-      y: cameraPosition.y,
-      z: cameraPosition.z,
-      onUpdate: () => camera.updateProjectionMatrix(),
-      onComplete: () => setOrbitControlsEnabled(true),
-    });
+    setNavigationItemClicked(true);
+    moveCamera(camera, item.screenPosition, item.screenRotation, () =>
+      openDisplayMenus(true)
+    );
   };
 
   return (
